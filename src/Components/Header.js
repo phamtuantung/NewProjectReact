45dc69a5f5650671338d8ccb9ccddb2e9f6503f1
Ptuantung <ptuantung@gmail.com>
2020-01-07 23:19:56 +0700

    commit lan 2 -_-

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React,{Component} from 'react';
 // import logo from './logo.svg';
 import './Header.css';
+import logo from './spotify.png';
 
 function Header() {
   return (
     <div class="Container">
-      <a href="https://www.spotify.com/vn-vi/"> Spotify logo</a>
+      {/* <a href="https://www.spotify.com/vn-vi/" ><img src={logo} width="132" height="40" /> </a> */}
+      <img src={logo} alt="logo" />
       <nav role="navigation">
         <ul>
-          <li href="https://www.spotify.com/vn-vi/premium/?checkout=false" class="">Premium</li>
-          <li href="https://support.spotify.com/">Trợ giúp</li>
-          <li href="https://www.spotify.com/vn-vi/download/">Tải xuống</li>
-          <li href="https://www.spotify.com/vn-vi/signup/">Đăng kí</li>
-          <li href="https://www.spotify.com/vn-vi/account/overview/">Đăng nhập</li>
+          <li href="https://www.spotify.com/vn-vi/premium/?checkout=false" class="Premium">Premium</li>
+          <li href="https://support.spotify.com/" class="">Trợ giúp</li>
+          <li href="https://www.spotify.com/vn-vi/download/" class="">Tải xuống</li>
+          <li href="https://www.spotify.com/vn-vi/signup/" class="">Đăng kí</li>
+          <li href="https://www.spotify.com/vn-vi/account/overview/" class="">Đăng nhập</li>
         </ul> 
       </nav>
-
     </div>
   );
 }
